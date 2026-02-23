cff85fbd5b4c66d94f59645155a3e58c58076cfe
vedant-2705 <vedant.daryapurkar@kanakasoftware.com>
2026-02-23 18:51:57 +0530

    fix: update phone number regex to restrict length to 10 digits

--- src/presentation/validators/contactValidator.ts
+++ src/presentation/validators/contactValidator.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-const phoneRegex = /^[+]?[(]?[0-9]{1,4}[)]?[-\s./0-9]{6,14}[0-9]$/;
+const phoneRegex = /^[0-9]{1,10}$/;
 
 export const createContactSchema = z.object({
     name: z
